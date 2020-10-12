1fbeeab05244687501286cd5005022b265e49ab9
HaoJing <hjing098@uottawa.ca>
2020-10-12 10:42:24 -0400

    Done

--- app/src/main/java/com/example/calculator_lab3/MainActivity.java
+++ app/src/main/java/com/example/calculator_lab3/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.calculator_lab3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button0, button1, button2, button3, button4, button5,button6, button7, button8,
+            button9, buttonAdd, buttonSub, buttonDivision, buttonMul, button10, buttonC,
+            buttonEqual;
+    TextView edttxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
