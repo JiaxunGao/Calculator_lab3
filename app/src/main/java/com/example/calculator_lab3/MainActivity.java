bfb396dcadc0727aa185d36ba76629cf5dcdbddc
JiaxunGao <3056751953@qq.com>
2020-10-12 11:02:36 -0400

    correct some functions

--- app/src/main/java/com/example/calculator_lab3/MainActivity.java
+++ app/src/main/java/com/example/calculator_lab3/MainActivity.java
@@ -6,32 +6,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-<<<<<<< HEAD
 
-public class MainActivity extends AppCompatActivity {
-    Button button0, button1, button2, button3, button4, button5,button6, button7, button8,
-            button9, buttonAdd, buttonSub, buttonDivision, buttonMul, button10, buttonC,
-            buttonEqual;
-    TextView edttxt;
-=======
 
 
 public class MainActivity extends AppCompatActivity {
     Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9,
-            buttonAdd, buttonSub, buttonDivison, buttonMul, buttonC, buttonEqual;
+            buttonAdd, buttonSub, buttonDivision, buttonMul, buttonC, buttonEqual;
 
     TextView text;
     float value1, value2;
     boolean add, sub, div, mul;
 
->>>>>>> 8725310079ef799c19d6f24d4c552dfdb04d466c
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        //constract buttons and the other elements here, name are given above//
+        //construct buttons and the other elements here, name are given above//
 
         button1.setOnClickListener((v)->{
             text.setText(text.getText() + "1");
